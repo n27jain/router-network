effafb5412f41c3638ae4d21f5d919531390606a
Naman Jain <n27jain@edu.uwaterloo.ca>
2023-05-09 17:11:20 -0400

    communication works locally

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use( express.json() );
+var tables = new Object();
+// app.use(express.json() ); 
+
+app.listen(
+    PORT,
+    () => console.log(`its alive on http://localhost:${PORT}`)
+)
+
+app.get('/infotest', (req, res) => {
+    res.status(200).send({
+        tshirt: "naman",
+        color: "red"
+    });
+
+});
+
+
+// app.post('addTable/:id', (req, res) => {
+
+// }
+app.post('/addTable/:device_id', (req, res) => {
+
+
+    const { device_id } = req.params; // This is the ID of the device sending its table 
+    var toDo = true;
+    const { routes } = req.body;
+    console.log(device_id, routes);
+    const table =  {
+        routes: routes
+    }
+    
+    tables[device_id] = table;
+
+    console.log("newObj:", tables);
+   
+
+    if(Object.keys(tables).length == 5){
+        // we are assuming that we are waiting for the routing for 5 different esp modules
+        console.log()
+        res.send({
+            message: "we have 5 routes and can now run the python script! done it!",
+            newObj: tables
+        });
+    }
+    else{
+        res.send({
+            message: "we done it!",
+            newObj: tables
+        });
+    }
+
+   
+})
